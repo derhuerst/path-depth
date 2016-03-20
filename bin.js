2836abc315c48c1f609845c7152206ed5f48243c
Jannis R <mail@jannisr.de>
2016-03-20 18:06:26 +0100

    remove CLI executable

--- bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict'
-
-const getStdin = require('get-stdin')
-const depth =    require('./index')
-
-getStdin().then(function (path) {
-	process.stdout.write(depth(path).toString() + '\n')
-})
